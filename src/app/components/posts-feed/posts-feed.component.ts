8e4a7a09234428c6d10622fb88f61cde898d7001
unknown <rellowy@gmail.com>
2022-07-30 15:19:25 +0300

    [30.07.22] - 15:19

--- src/app/components/posts-feed/posts-feed.component.ts
+++ src/app/components/posts-feed/posts-feed.component.ts
@@ -1,5 +1,5 @@
 import { PostsService } from './../../services/posts.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import IPost from 'src/app/models/interfaces/IPost';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
@@ -14,8 +14,13 @@ export class PostsFeedComponent implements OnInit {
   @ViewChild(CdkVirtualScrollViewport)
   viewport!: CdkVirtualScrollViewport;
 
+  @ViewChild('list')
+  list!: ElementRef;
+
   batch = 10;
   theEnd = false;
+  itemSize = 686;
+  maxItemSize = 686;
 
   offset = new BehaviorSubject(0);
   infinite!: Observable<any[]>;
@@ -43,7 +48,7 @@ export class PostsFeedComponent implements OnInit {
   nextBatch(e: any, offset: number) {
     const end = this.viewport.getRenderedRange().end;
     const total = this.viewport.getDataLength();
-    console.log(`${end}, '>=', ${total}`);
+    // console.log(`${end}, '>=', ${total}`);
     if (end === total) {
       this.theEnd = true;
       this.offset.next(offset);
@@ -55,4 +60,15 @@ export class PostsFeedComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  ngAfterViewChecked(): void {
+    if (this.list?.nativeElement?.children[0]?.children[0]?.children[0]) {
+      const a = this.list.nativeElement.children[0].children[0].children[0];
+      const b = a.getBoundingClientRect().height;
+      if (this.itemSize !== b && b <= this.maxItemSize) {
+        this.itemSize = b;
+        console.log(this.itemSize);
+      }
+    }
+  }
 }
